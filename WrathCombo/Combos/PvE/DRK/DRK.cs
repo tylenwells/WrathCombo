a105e17c64256a9be89904ded79e2c5e40007263
Ethan Henderson <ethan@zbee.codes>
2025-02-03 16:03:57 -0700

    Revert "Add interrupt options to DRK"
    This reverts commit 475786b1091d6e6ec8ebc12b44348960ae5ffd7c.

--- WrathCombo/Combos/PvE/DRK/DRK.cs
+++ WrathCombo/Combos/PvE/DRK/DRK.cs
@@ -47,12 +47,6 @@ internal partial class DRK
 
             #endregion
 
-            // Interrupt
-            if (IsEnabled(CustomComboPreset.DRK_ST_Interrupt)
-                && ActionReady(All.Interject)
-                && CanInterruptEnemy())
-                return All.Interject;
-
             // Variant Cure - Heal: Priority to save your life
             if (IsEnabled(CustomComboPreset.DRK_Variant_Cure)
                 && IsEnabled(Variant.VariantCure)
@@ -328,18 +322,6 @@ internal partial class DRK
                 && PlayerHealthPercentageHp() <=
                 GetOptionValue(Config.DRK_VariantCure))
                 return Variant.VariantCure;
-
-            // Interrupt
-            if (IsEnabled(CustomComboPreset.DRK_AoE_Interrupt)
-                && ActionReady(All.Interject)
-                && CanInterruptEnemy())
-                return All.Interject;
-
-            // Stun
-            if (IsEnabled(CustomComboPreset.DRK_AoE_Stun)
-                && ActionReady(All.LowBlow)
-                && TargetIsCasting())
-                return All.LowBlow;
 
             // Disesteem
             if (LevelChecked(LivingShadow)
