4198ef7030042be2c4e7c6d019e925f9e905c990
Ethan Henderson <ethan@zbee.codes>
2025-02-15 05:11:50 -0700

    Reformat `ActionReplacer`

--- WrathCombo/Core/ActionReplacer.cs
+++ WrathCombo/Core/ActionReplacer.cs
@@ -14,135 +14,153 @@ using WrathCombo.CustomComboNS.Functions;
 using WrathCombo.Extensions;
 using WrathCombo.Services;
 
-namespace WrathCombo.Core
+namespace WrathCombo.Core;
+
+/// <summary> This class facilitates action+icon replacement. </summary>
+internal sealed class ActionReplacer : IDisposable
 {
-    /// <summary> This class facilitates action+icon replacement. </summary>
-    internal sealed class ActionReplacer : IDisposable
+    public readonly List<CustomCombo> CustomCombos;
+
+    private readonly Hook<IsActionReplaceableDelegate> isActionReplaceableHook;
+    public readonly Hook<GetActionDelegate> getActionHook;
+
+    private delegate ulong IsActionReplaceableDelegate(uint actionID);
+    public delegate uint GetActionDelegate(uint actionID);
+
+    private ulong IsActionReplaceableDetour(uint actionID) => 1;
+
+    private readonly Dictionary<uint, uint> lastActionInvokeFor = [];
+
+    /// <summary> Calls the original hook. </summary>
+    /// <param name="actionID"> Action ID. </param>
+    /// <returns> The result from the hook. </returns>
+    internal uint OriginalHook(uint actionID) => getActionHook.Original(actionID);
+
+    /// <summary> Initializes a new instance of the <see cref="ActionReplacer"/> class. </summary>
+    public ActionReplacer()
     {
-        public readonly List<CustomCombo> CustomCombos;
-        private readonly Hook<IsActionReplaceableDelegate> isActionReplaceableHook;
-        public readonly Hook<GetActionDelegate> getActionHook;
+        CustomCombos = Assembly.GetAssembly(typeof(CustomCombo))!.GetTypes()
+            .Where(t => !t.IsAbstract && t.BaseType == typeof(CustomCombo))
+            .Select(Activator.CreateInstance)
+            .Cast<CustomCombo>()
+            .OrderByDescending(x => x.Preset)
+            .ToList();
 
-        private IntPtr actionManager = IntPtr.Zero;
-        private readonly IntPtr module = IntPtr.Zero;
-        private Dictionary<uint, uint> lastActionInvokeFor = new Dictionary<uint, uint>();
+        getActionHook = Svc.Hook.HookFromAddress<GetActionDelegate>
+            (ActionManager.Addresses.GetAdjustedActionId.Value, GetAdjustedActionDetour);
+        isActionReplaceableHook = Svc.Hook.HookFromAddress<IsActionReplaceableDelegate>
+            (Service.Address.IsActionIdReplaceable, IsActionReplaceableDetour);
 
-        /// <summary> Initializes a new instance of the <see cref="ActionReplacer"/> class. </summary>
-        public ActionReplacer()
-        {
-            CustomCombos = Assembly.GetAssembly(typeof(CustomCombo))!.GetTypes()
-                .Where(t => !t.IsAbstract && t.BaseType == typeof(CustomCombo))
-                .Select(t => Activator.CreateInstance(t))
-                .Cast<CustomCombo>()
-                .OrderByDescending(x => x.Preset)
-                .ToList();
+        getActionHook.Enable();
+        isActionReplaceableHook.Enable();
+    }
 
-            getActionHook = Svc.Hook.HookFromAddress<GetActionDelegate>((nint)ActionManager.Addresses.GetAdjustedActionId.Value, GetAdjustedActionDetour);
-            isActionReplaceableHook = Svc.Hook.HookFromAddress<IsActionReplaceableDelegate>(Service.Address.IsActionIdReplaceable, IsActionReplaceableDetour);
+    /// <summary>
+    ///     Throttles access to <see cref="GetAdjustedAction(uint)"/>.
+    /// </summary>
+    /// <param name="actionID">The action a combo replaces.</param>
+    /// <returns>The action a combo returns.</returns>
+    private uint GetAdjustedActionDetour(uint actionID)
+    {
+        if (FilteredCombos is null)
+            UpdateFilteredCombos();
 
-            getActionHook.Enable();
-            isActionReplaceableHook.Enable();
-        }
-
-        private delegate ulong IsActionReplaceableDelegate(uint actionID);
-
-        public delegate uint GetActionDelegate(IntPtr actionManager, uint actionID);
-
-        /// <inheritdoc/>
-        public void Dispose()
-        {
-            getActionHook?.Disable();
-            getActionHook?.Dispose();
-            isActionReplaceableHook?.Disable();
-            isActionReplaceableHook?.Dispose();
-        }
-
-        /// <summary> Calls the original hook. </summary>
-        /// <param name="actionID"> Action ID. </param>
-        /// <returns> The result from the hook. </returns>
-        internal uint OriginalHook(uint actionID) => getActionHook.Original(actionManager, actionID);
-
-        public static IEnumerable<CustomCombo>? FilteredCombos;
-
-        public void UpdateFilteredCombos()
-        {
-            FilteredCombos = CustomCombos.Where(x => x.Preset.Attributes() is not null && x.Preset.Attributes().IsPvP == CustomComboFunctions.InPvP() && ((x.Preset.Attributes().RoleAttribute is not null && x.Preset.Attributes().RoleAttribute.PlayerIsRole()) || x.Preset.Attributes().CustomComboInfo.JobID == Player.JobId || x.Preset.Attributes().CustomComboInfo.JobID == CustomComboFunctions.JobIDs.ClassToJob(Player.JobId)));
-            Svc.Log.Debug($"Now running {FilteredCombos.Count()} combos\n{string.Join("\n", FilteredCombos.Select(x => x.Preset.Attributes().CustomComboInfo.Name))}");
-        }
-
-        private uint GetAdjustedActionDetour(IntPtr _, uint actionID)
-        {
-            if (FilteredCombos is null)
-                UpdateFilteredCombos();
-
-            // Only refresh every so often
-            if (!EzThrottler.Throttle("Actions" + actionID,
-                    Service.Configuration.Throttle))
-                return lastActionInvokeFor[actionID];
-
-            // Actually get the action
-            lastActionInvokeFor[actionID] = GetAdjustedAction(actionID);
+        // Only refresh every so often
+        if (!EzThrottler.Throttle("Actions" + actionID,
+                Service.Configuration.Throttle))
             return lastActionInvokeFor[actionID];
-        }
 
-        private unsafe uint GetAdjustedAction(uint actionID)
+        // Actually get the action
+        lastActionInvokeFor[actionID] = GetAdjustedAction(actionID);
+        return lastActionInvokeFor[actionID];
+    }
+
+    /// <summary>
+    ///     Replaces an action with the result from a combo.
+    /// </summary>
+    /// <param name="actionID">The action a combo replaces.</param>
+    /// <returns>The action a combo returns.</returns>
+    private unsafe uint GetAdjustedAction(uint actionID)
+    {
+        try
         {
-            try
+            if (Service.Configuration.PerformanceMode)
+                return OriginalHook(actionID);
+
+            if (Svc.ClientState.LocalPlayer == null)
+                return OriginalHook(actionID);
+
+            if (ClassLocked() ||
+                (DisabledJobsPVE.Any(x => x == Svc.ClientState.LocalPlayer.ClassJob.RowId) && !Svc.ClientState.IsPvP) ||
+                (DisabledJobsPVP.Any(x => x == Svc.ClientState.LocalPlayer.ClassJob.RowId) && Svc.ClientState.IsPvP))
+                return OriginalHook(actionID);
+
+            foreach (CustomCombo? combo in FilteredCombos)
             {
-                if (Service.Configuration.PerformanceMode)
-                    return OriginalHook(actionID);
-
-                if (Svc.ClientState.LocalPlayer == null)
-                    return OriginalHook(actionID);
-
-                if (ClassLocked() ||
-                    (DisabledJobsPVE.Any(x => x == Svc.ClientState.LocalPlayer.ClassJob.RowId) && !Svc.ClientState.IsPvP) ||
-                    (DisabledJobsPVP.Any(x => x == Svc.ClientState.LocalPlayer.ClassJob.RowId) && Svc.ClientState.IsPvP))
-                    return OriginalHook(actionID);
-
-                foreach (CustomCombo? combo in FilteredCombos)
+                if (combo.TryInvoke(actionID, out uint newActionID))
                 {
-                    if (combo.TryInvoke(actionID, out uint newActionID))
+                    if (Service.Configuration.BlockSpellOnMove && ActionManager.GetAdjustedCastTime(ActionType.Action, newActionID) > 0 && CustomComboFunctions.TimeMoving.Ticks > 0)
                     {
-                        if (Service.Configuration.BlockSpellOnMove && ActionManager.GetAdjustedCastTime(ActionType.Action, newActionID) > 0 && CustomComboFunctions.TimeMoving.Ticks > 0)
-                        {
-                            return All.SavageBlade;
-                        }
-                        return newActionID;
+                        return All.SavageBlade;
                     }
+                    return newActionID;
                 }
-
-                return OriginalHook(actionID);
             }
 
-            catch (Exception ex)
-            {
-                Svc.Log.Error(ex, "Preset error");
-                return OriginalHook(actionID);
-            }
+            return OriginalHook(actionID);
         }
 
-        // Class locking
-        public unsafe static bool ClassLocked()
+        catch (Exception ex)
         {
-            if (Svc.ClientState.LocalPlayer is null) return false;
-
-            if (Svc.ClientState.LocalPlayer.Level <= 35) return false;
-
-            if (Svc.ClientState.LocalPlayer.ClassJob.RowId is
-                (>= 8 and <= 25) or 27 or 28 or >= 30)
-                return false;
-
-            if (!UIState.Instance()->IsUnlockLinkUnlockedOrQuestCompleted(66049))
-                return false;
-
-            if ((Svc.ClientState.LocalPlayer.ClassJob.RowId is 1 or 2 or 3 or 4 or 5 or 6 or 7 or 26 or 29) &&
-                Svc.Condition[Dalamud.Game.ClientState.Conditions.ConditionFlag.BoundByDuty] &&
-                Svc.ClientState.LocalPlayer.Level > 35) return true;
-
-            return false;
+            Svc.Log.Error(ex, "Preset error");
+            return OriginalHook(actionID);
         }
+    }
 
-        private ulong IsActionReplaceableDetour(uint actionID) => 1;
+    #region Restrict combos to current job
+
+    public static IEnumerable<CustomCombo>? FilteredCombos;
+
+    public void UpdateFilteredCombos()
+    {
+        FilteredCombos = CustomCombos.Where(x => x.Preset.Attributes() is not null && x.Preset.Attributes().IsPvP == CustomComboFunctions.InPvP() && ((x.Preset.Attributes().RoleAttribute is not null && x.Preset.Attributes().RoleAttribute.PlayerIsRole()) || x.Preset.Attributes().CustomComboInfo.JobID == Player.JobId || x.Preset.Attributes().CustomComboInfo.JobID == CustomComboFunctions.JobIDs.ClassToJob(Player.JobId)));
+        var filteredCombos = FilteredCombos as CustomCombo[] ?? FilteredCombos.ToArray();
+        Svc.Log.Debug($"Now running {filteredCombos.Count()} combos\n{string.Join("\n", filteredCombos.Select(x => x.Preset.Attributes().CustomComboInfo.Name))}");
+    }
+
+    #endregion
+
+    /// <summary>
+    ///     Checks if the player could be on a job instead of a class.
+    /// </summary>
+    /// <returns>
+    ///     <see langword="true"/> if the user could be on a job instead.
+    /// </returns>
+    public static unsafe bool ClassLocked()
+    {
+        if (Svc.ClientState.LocalPlayer is null) return false;
+
+        if (Svc.ClientState.LocalPlayer.Level <= 35) return false;
+
+        if (Svc.ClientState.LocalPlayer.ClassJob.RowId is
+            (>= 8 and <= 25) or 27 or 28 or >= 30)
+            return false;
+
+        if (!UIState.Instance()->IsUnlockLinkUnlockedOrQuestCompleted(66049))
+            return false;
+
+        if ((Svc.ClientState.LocalPlayer.ClassJob.RowId is 1 or 2 or 3 or 4 or 5 or 6 or 7 or 26 or 29) &&
+            Svc.Condition[Dalamud.Game.ClientState.Conditions.ConditionFlag.BoundByDuty] &&
+            Svc.ClientState.LocalPlayer.Level > 35) return true;
+
+        return false;
+    }
+
+    public void Dispose()
+    {
+        getActionHook.Disable();
+        getActionHook.Dispose();
+        isActionReplaceableHook.Disable();
+        isActionReplaceableHook.Dispose();
     }
 }
