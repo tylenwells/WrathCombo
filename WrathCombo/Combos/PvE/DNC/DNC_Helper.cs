7647564cbe932c41184a9a268a94865ac6526b38
Ethan Henderson <ethan@zbee.codes>
2025-02-24 13:27:56 -0700

    Add Dance Partner determination logic

--- WrathCombo/Combos/PvE/DNC/DNC_Helper.cs
+++ WrathCombo/Combos/PvE/DNC/DNC_Helper.cs
@@ -1,18 +1,18 @@
 ﻿#region
 
-using Dalamud.Game.ClientState.JobGauge.Types;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Dalamud.Game.ClientState.JobGauge.Types;
 using Dalamud.Game.ClientState.Objects.Types;
+using ECommons.DalamudServices;
+using ECommons.GameHelpers;
 using WrathCombo.CustomComboNS;
 using WrathCombo.CustomComboNS.Functions;
+using WrathCombo.Extensions;
 using WrathCombo.Services;
 using static WrathCombo.CustomComboNS.Functions.CustomComboFunctions;
 using Options = WrathCombo.Combos.CustomComboPreset;
-
-// ReSharper disable ClassNeverInstantiated.Global
-// ReSharper disable CheckNamespace
 
 #endregion
 
@@ -29,40 +29,6 @@ namespace WrathCombo.Combos.PvE;
 internal partial class DNC
 {
     /// <summary>
-    ///     Logic to pick different openers.
-    /// </summary>
-    /// <returns>The chosen Opener.</returns>
-    internal static WrathOpener Opener()
-    {
-        if (Config.DNC_ST_OpenerSelection ==
-            (int) Config.Openers.FifteenSecond &&
-            Opener15S.LevelChecked)
-            return Opener15S;
-
-        if (Config.DNC_ST_OpenerSelection ==
-            (int) Config.Openers.SevenSecond &&
-            Opener07S.LevelChecked)
-            return Opener07S;
-
-        if (Config.DNC_ST_OpenerSelection ==
-            (int) Config.Openers.ThirtySecondTech &&
-            Opener30STech.LevelChecked)
-            return Opener30STech;
-
-        if (Config.DNC_ST_OpenerSelection ==
-            (int) Config.Openers.SevenPlusSecondTech &&
-            Opener07PlusSTech.LevelChecked)
-            return Opener07PlusSTech;
-
-        if (Config.DNC_ST_OpenerSelection ==
-            (int) Config.Openers.SevenSecondTech &&
-            Opener07STech.LevelChecked)
-            return Opener07STech;
-
-        return WrathOpener.Dummy;
-    }
-
-    /// <summary>
     ///     Dancer Gauge data, just consolidated.
     /// </summary>
     private static DNCGauge Gauge => GetJobGauge<DNCGauge>();
@@ -72,6 +38,40 @@ internal partial class DNC
     /// </summary>
     private static double GCD =>
         Math.Floor(GetCooldown(Cascade).CooldownTotal * 100) / 100;
+
+    /// <summary>
+    ///     Logic to pick different openers.
+    /// </summary>
+    /// <returns>The chosen Opener.</returns>
+    internal static WrathOpener Opener()
+    {
+        if (Config.DNC_ST_OpenerSelection ==
+            (int)Config.Openers.FifteenSecond &&
+            Opener15S.LevelChecked)
+            return Opener15S;
+
+        if (Config.DNC_ST_OpenerSelection ==
+            (int)Config.Openers.SevenSecond &&
+            Opener07S.LevelChecked)
+            return Opener07S;
+
+        if (Config.DNC_ST_OpenerSelection ==
+            (int)Config.Openers.ThirtySecondTech &&
+            Opener30STech.LevelChecked)
+            return Opener30STech;
+
+        if (Config.DNC_ST_OpenerSelection ==
+            (int)Config.Openers.SevenPlusSecondTech &&
+            Opener07PlusSTech.LevelChecked)
+            return Opener07PlusSTech;
+
+        if (Config.DNC_ST_OpenerSelection ==
+            (int)Config.Openers.SevenSecondTech &&
+            Opener07STech.LevelChecked)
+            return Opener07STech;
+
+        return WrathOpener.Dummy;
+    }
 
     /// <summary>
     ///     Check if the rotation is in Auto-Rotation.
@@ -102,23 +102,232 @@ internal partial class DNC
 
     #region Dance Partner
 
+    internal static ulong? CurrentDancePartner =>
+        GetPartyMembers()
+            .Where(HasMyPartner)
+            .Select(x => (ulong?)x.GameObjectId)
+            .FirstOrDefault();
+
     internal static ulong? DesiredDancePartner =>
         TryGetDancePartner(out var partner) ? partner.GameObjectId : null;
 
-    private static bool TryGetDancePartner(out IGameObject? partner)
+    private static bool TryGetDancePartner
+        (out IGameObject? partner, bool? callingFromFeature = null)
     {
         partner = null;
+        var playerID = LocalPlayer.GameObjectId;
+        var party = GetPartyMembers()
+            .Where(member => member.GameObjectId != playerID)
+            .Where(member => !member.BattleChara.IsDead)
+            .Where(member => IsInRange(member.BattleChara, 30))
+            .Where(member => !HasAnyPartner(member) || HasMyPartner(member))
+            .Select(member => member.BattleChara)
+            .ToList();
 
-        if (!IsEnabled(Options.DNC_ST_Adv_Partner))
+        // Bails
+        if (!Player.Available)
+            return false;
+        if (party.Count <= 1 && !HasCompanionPresent())
             return false;
 
-        if (!HasEffect(Buffs.ClosedPosition))
-            return false;
+        // Check if we have a target overriding any searching
+        /*
+         if (callingFromFeature is true &&
+            IsEnabled(Options.DNC_Desirable_TargetOverride) &&
+            LocalPlayer.TargetObject is IBattleChara &&
+            !LocalPlayer.TargetObject.IsDead &&
+            party.Any(x =>
+                x.GameObjectId == LocalPlayer.TargetObject.GameObjectId) &&
+            IsInRange(LocalPlayer.TargetObject, 30))
+        {
+            partner = LocalPlayer.TargetObject;
+            return true;
+        }*/
 
-        partner = GetPartyMembers();
+        // Search for a partner
+        if (TryGetBestPartner(out var bestPartner))
+        {
+            partner = bestPartner;
+            return true;
+        }
 
-        return partner != null;
+        // Fallback to companion
+        if (HasCompanionPresent())
+        {
+            partner = Svc.Buddies.CompanionBuddy.GameObject;
+            return true;
+        }
+
+        // Fallback to first party slot that isn't the player
+        if (party.Count > 1)
+        {
+            partner = party.First();
+            return true;
+        }
+
+        return false;
+
+        #region Sickness-checking shortcut methods
+
+        bool SicknessFree(IGameObject target)
+        {
+            return !TargetHasRezWeakness(target);
+        }
+
+        bool BrinkFree(IGameObject target)
+        {
+            return !TargetHasRezWeakness(target, false);
+        }
+
+        #endregion
+
+        bool TryGetBestPartner(out IGameObject? bestPartner, int step = 0)
+        {
+            #region Variable Setup
+
+            bestPartner = null;
+            var restrictions = PartnerPriority.RestrictionSteps[step];
+            var filter = party;
+            const int melee = (int)PartnerPriority.Role.Melee;
+            const int ranged = (int)PartnerPriority.Role.Ranged;
+
+            #endregion
+
+            if (restrictions.HasFlag(PartnerPriority.Restrictions.MustBeMelee))
+                filter = filter
+                    .Where(x => x.ClassJob.RowId.Role() is melee).ToList();
+
+            if (restrictions.HasFlag(PartnerPriority.Restrictions.MustBeDPS))
+                filter = filter
+                    .Where(x => x.ClassJob.RowId.Role() is melee or ranged)
+                    .ToList();
+
+            if (restrictions.HasFlag(PartnerPriority.Restrictions
+                    .MustBeSicknessFree))
+                filter = filter.Where(SicknessFree).ToList();
+
+            if (restrictions.HasFlag(PartnerPriority.Restrictions.MustBeBrinkFree))
+                filter = filter.Where(BrinkFree).ToList();
+
+            if (filter.Count == 0 &&
+                step < PartnerPriority.RestrictionSteps.Length - 1)
+                return TryGetBestPartner(out bestPartner, step + 1);
+            if (filter.Count == 0 && step == 6)
+                return false;
+
+            filter = filter
+                .OrderBy(x =>
+                    PartnerPriority.RolePrio.GetValueOrDefault(
+                        x.ClassJob.RowId.Role(), int.MaxValue))
+                .ThenBy(x =>
+                    Player.Level >= 90
+                        ? PartnerPriority.Job090Prio.GetValueOrDefault(
+                            x.ClassJob.RowId, int.MaxValue)
+                        : int.MaxValue)
+                .ThenBy(x =>
+                    Player.Level >= 100
+                        ? PartnerPriority.Job100Prio.GetValueOrDefault(
+                            x.ClassJob.RowId, int.MaxValue)
+                        : int.MaxValue)
+                .ToList();
+
+            bestPartner = filter.First();
+            return true;
+        }
     }
+
+    private static bool HasAnyPartner(WrathPartyMember target) =>
+        FindEffect(Buffs.Partner, target.BattleChara, null)
+            is not null;
+
+    private static bool HasMyPartner(WrathPartyMember target) =>
+        FindEffect(Buffs.Partner, target.BattleChara, LocalPlayer?.GameObjectId)
+            is not null;
+
+    #region Partner Priority Static Data
+
+    private static class PartnerPriority
+    {
+        internal static readonly Dictionary<int, int> RolePrio = new()
+        {
+            { (int)Role.Melee, 1 },
+            { (int)Role.Ranged, 1 },
+            { (int)Role.Tank, 2 },
+            { (int)Role.Healer, 3 },
+        };
+
+        internal static readonly Dictionary<uint, int> Job100Prio = new()
+        {
+            { PCT.JobID, 1 },
+            { SAM.JobID, 1 },
+            { RPR.JobID, 2 },
+            { VPR.JobID, 2 },
+            { MNK.JobID, 2 },
+            { NIN.JobID, 2 },
+            { DRG.JobID, 3 },
+            { BLM.JobID, 3 },
+            { RDM.JobID, 4 },
+            { SMN.JobID, 5 },
+            { MCH.JobID, 6 },
+            { BRD.JobID, 7 },
+            { JobID, 8 },
+        };
+
+        internal static readonly Dictionary<uint, int> Job090Prio = new()
+        {
+            { PCT.JobID, 0 },
+            { SAM.JobID, 1 },
+            { NIN.JobID, 2 },
+            { MNK.JobID, 3 },
+            { RPR.JobID, 4 },
+            { BLM.JobID, 5 },
+            { DRG.JobID, 6 },
+            { VPR.JobID, 7 },
+            { SMN.JobID, 8 },
+            { RDM.JobID, 9 },
+            { MCH.JobID, 10 },
+            { BRD.JobID, 11 },
+            { JobID, 12 },
+        };
+
+        internal static readonly Restrictions[] RestrictionSteps =
+        [
+            // Sickness-free DPS
+            Restrictions.MustBeMelee | Restrictions.MustBeSicknessFree,
+            Restrictions.MustBeDPS | Restrictions.MustBeSicknessFree,
+            // Sick DPS
+            Restrictions.MustBeMelee | Restrictions.MustBeBrinkFree,
+            Restrictions.MustBeDPS | Restrictions.MustBeBrinkFree,
+            // Sickness-free
+            Restrictions.MustBeSicknessFree,
+            // Sick
+            Restrictions.MustBeBrinkFree,
+            // :(
+            Restrictions.ScrapeTheBottom,
+        ];
+
+        internal enum Role
+        {
+            Tank = 1,
+            Melee = 2,
+
+            /// Casters and Phys Ranged
+            Ranged = 3,
+            Healer = 4,
+        }
+
+        [Flags]
+        internal enum Restrictions
+        {
+            MustBeMelee = 1 << 0, // 1
+            MustBeDPS = 1 << 1, // 2
+            MustBeSicknessFree = 1 << 2, // 4
+            MustBeBrinkFree = 1 << 3, // 8
+            ScrapeTheBottom = 1 << 4, // 16
+        }
+    }
+
+    #endregion
 
     #endregion
 
@@ -145,8 +354,8 @@ internal partial class DNC
     /// </summary>
     /// <param name="action">The action ID to check.</param>
     /// <param name="updatedAction">
-    ///     The matching dance step the action was assigned to.<br/>
-    ///     Will be Savage Blade if used and was not a custom dance step.<br/>
+    ///     The matching dance step the action was assigned to.<br />
+    ///     Will be Savage Blade if used and was not a custom dance step.<br />
     ///     Do not use this value if the return is <c>false</c>.
     /// </param>
     /// <returns>If the action was assigned as a custom dance step.</returns>
@@ -187,7 +396,6 @@ internal partial class DNC
     internal class FifteenSecondOpener : WrathOpener
     {
         public override int MinOpenerLevel => 100;
-
         public override int MaxOpenerLevel => 109;
 
         public override List<uint> OpenerActions { get; set; } =
@@ -279,7 +487,6 @@ internal partial class DNC
     internal class SevenSecondOpener : WrathOpener
     {
         public override int MinOpenerLevel => 100;
-
         public override int MaxOpenerLevel => 109;
 
         public override List<uint> OpenerActions { get; set; } =
@@ -375,7 +582,6 @@ internal partial class DNC
     internal class ThirtySecondTechOpener : WrathOpener
     {
         public override int MinOpenerLevel => 100;
-
         public override int MaxOpenerLevel => 109;
 
         public override List<uint> OpenerActions { get; set; } =
@@ -467,7 +673,6 @@ internal partial class DNC
     internal class SevenPlusSecondTechOpener : WrathOpener
     {
         public override int MinOpenerLevel => 100;
-
         public override int MaxOpenerLevel => 109;
 
         public override List<uint> OpenerActions { get; set; } =
@@ -550,7 +755,6 @@ internal partial class DNC
     internal class SevenSecondTechOpener : WrathOpener
     {
         public override int MinOpenerLevel => 100;
-
         public override int MaxOpenerLevel => 109;
 
         public override List<uint> OpenerActions { get; set; } =
@@ -678,6 +882,7 @@ internal partial class DNC
         Peloton = 7557,
         SaberDance = 16005,
         ClosedPosition = 16006,
+        Ending = 18073,
         EnAvant = 16010,
         Devilment = 16011,
         ShieldSamba = 16012,
@@ -716,6 +921,7 @@ internal partial class DNC
             // Other
             Peloton = 1199,
             ClosedPosition = 1823,
+            Partner = 1824,
             ShieldSamba = 1826,
             LastDanceReady = 3867,
             FinishingMoveReady = 3868,
