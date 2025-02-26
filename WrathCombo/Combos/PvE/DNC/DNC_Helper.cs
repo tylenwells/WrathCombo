540632205544c7708a722a826934b677679624c4
Ethan Henderson <ethan@zbee.codes>
2025-02-26 07:49:38 -0700

    Categorize the user's GCD speed

--- WrathCombo/Combos/PvE/DNC/DNC_Helper.cs
+++ WrathCombo/Combos/PvE/DNC/DNC_Helper.cs
@@ -151,6 +151,25 @@ internal partial class DNC
         return desiredFinish;
     }
 
+    #region GCD Evaluation
+
+    private static GCDRange GCDValue =>
+        GCD switch
+        {
+            2.50 => GCDRange.Perfect,
+            2.49 => GCDRange.NotGood,
+            _ => GCDRange.Bad,
+        };
+
+    private enum GCDRange
+    {
+        Perfect,
+        NotGood,
+        Bad,
+    }
+
+    #endregion
+
     #region Custom Dance Step Logic
 
     /// <summary>
