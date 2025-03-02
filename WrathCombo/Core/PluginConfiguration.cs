6f1ab2c5d7d92a0d1c9c2f600e1b413bf6750cda
Ethan Henderson <ethan@zbee.codes>
2025-03-02 06:22:22 -0700

    Limit `SetActionChanging`, fixes saving every tick

--- WrathCombo/Core/PluginConfiguration.cs
+++ WrathCombo/Core/PluginConfiguration.cs
@@ -69,18 +69,23 @@ namespace WrathCombo.Core
 
         public bool ActionChanging = true;
 
+        private DateTime _lastActionChangeCheck = DateTime.MinValue;
+
         internal void SetActionChanging(bool? newValue = null)
         {
-            if (newValue is not null)
+            if ((DateTime.Now - _lastActionChangeCheck).TotalSeconds < 3) return;
+
+            if (newValue is not null && newValue != ActionChanging)
+            {
                 ActionChanging = newValue.Value;
+                Save();
+            }
 
             // Checks if action replacing is not in line with the setting
             if (ActionChanging && !Service.ActionReplacer.getActionHook.IsEnabled)
                 Service.ActionReplacer.getActionHook.Enable();
             if (!ActionChanging && Service.ActionReplacer.getActionHook.IsEnabled)
                 Service.ActionReplacer.getActionHook.Disable();
-
-            Save();
         }
 
         #endregion
