e7348bf5fe38099053001a7dbecd03d9bf08f092
Taurenkey <github@taurenkey.co.uk>
2025-03-16 07:42:27 +0000

    Debuglog stuff

--- WrathCombo/Window/Functions/Presets.cs
+++ WrathCombo/Window/Functions/Presets.cs
@@ -95,6 +95,7 @@ namespace WrathCombo.Window.Functions
                 if (P.UIHelper.ShowIPCControlledCheckboxIfNeeded
                         ($"###AutoAction{preset}", ref autoOn, preset, false))
                 {
+                    DebugFile.AddLog($"Set Auto-Mode for {preset} to {autoOn}");
                     Service.Configuration.AutoActions[preset] = autoOn;
                     Service.Configuration.Save();
                 }
@@ -120,6 +121,8 @@ namespace WrathCombo.Window.Functions
                 {
                     PresetStorage.DisablePreset(preset);
                 }
+
+                DebugFile.AddLog($"Set {preset} to {enabled}");
 
                 Service.Configuration.Save();
             }
