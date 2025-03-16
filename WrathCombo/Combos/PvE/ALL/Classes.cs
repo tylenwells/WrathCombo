611f04dbf1c906666fac9b9f76caf6edebaa4aa0
Tartarga <109563717+Tartarga@users.noreply.github.com>
2025-03-15 21:33:58 -0500

    Added some comments

--- WrathCombo/Combos/PvE/ALL/Classes.cs
+++ WrathCombo/Combos/PvE/ALL/Classes.cs
@@ -2,6 +2,10 @@
 
 namespace WrathCombo.Combos.PvE
 {
+    //This defines a FFXIV job type, and maps specific Role and Variant actions to that job
+    //Examples
+    // GNB.Role.Interject would work, SGE.Role.Interject would not.
+    //THis should help for future jobs and future random actions to quickly wireup job appropriate actions
     class Healer
     {
         public class Variant : VariantHealer;
