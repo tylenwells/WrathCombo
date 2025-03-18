a0fc643bc31ea19977f8a9779a0fd614893304cf
Kage <95875972+Kagekazu@users.noreply.github.com>
2025-03-18 11:44:29 +0100

    better SoD

--- WrathCombo/Combos/PvE/RPR/RPR_Helper.cs
+++ WrathCombo/Combos/PvE/RPR/RPR_Helper.cs
@@ -81,49 +81,72 @@ internal partial class RPR
             !HasEffect(Buffs.ImmortalSacrifice) && !IsComboExpiring(3) &&
             !JustUsed(ShadowOfDeath))
         {
-            if (IsEnabled(CustomComboPreset.RPR_ST_SimpleMode) ||
-                IsEnabled(CustomComboPreset.RPR_ST_AdvancedMode) &&
-                (Config.RPR_ST_ArcaneCircle_SubOption == 0 ||
-                 Config.RPR_ST_ArcaneCircle_SubOption == 1 && InBossEncounter()))
+            if (IsEnabled(CustomComboPreset.RPR_ST_SimpleMode))
             {
-                //1st part double enshroud
-                if (LevelChecked(PlentifulHarvest) && HasEffect(Buffs.Enshrouded) &&
-                    GetCooldownRemainingTime(ArcaneCircle) <= GCD * 2 + 1.5 && JustUsed(Enshroud))
+                if (!InBossEncounter() && LevelChecked(PlentifulHarvest) && !HasEffect(Buffs.Enshrouded) &&
+                    GetDebuffRemainingTime(Debuffs.DeathsDesign) <= 8)
                     return true;
 
-                //2nd part double enshroud
-                if (LevelChecked(PlentifulHarvest) && HasEffect(Buffs.Enshrouded) &&
-                    (GetCooldownRemainingTime(ArcaneCircle) <= GCD || IsOffCooldown(ArcaneCircle)) &&
-                    (JustUsed(VoidReaping) || JustUsed(CrossReaping)))
-                    return true;
+                if (InBossEncounter())
+                {
+                    //1st part double enshroud
+                    if (LevelChecked(PlentifulHarvest) && HasEffect(Buffs.Enshrouded) &&
+                        GetCooldownRemainingTime(ArcaneCircle) <= GCD * 2 + 1.5 && JustUsed(Enshroud))
+                        return true;
 
-                //lvl 88+ general use
-                if (LevelChecked(PlentifulHarvest) && !HasEffect(Buffs.Enshrouded) &&
-                    (IsEnabled(CustomComboPreset.RPR_ST_SimpleMode) &&
-                     GetDebuffRemainingTime(Debuffs.DeathsDesign) <= 8 ||
-                     IsEnabled(CustomComboPreset.RPR_ST_AdvancedMode) &&
-                     GetDebuffRemainingTime(Debuffs.DeathsDesign) <= Config.RPR_SoDRefreshRange) &&
-                    (GetCooldownRemainingTime(ArcaneCircle) > GCD * 8 || IsOffCooldown(ArcaneCircle)))
-                    return true;
+                    //2nd part double enshroud
+                    if (LevelChecked(PlentifulHarvest) && HasEffect(Buffs.Enshrouded) &&
+                        (GetCooldownRemainingTime(ArcaneCircle) <= GCD || IsOffCooldown(ArcaneCircle)) &&
+                        (JustUsed(VoidReaping) || JustUsed(CrossReaping)))
+                        return true;
 
-                //below lvl 88 use
-                if (!LevelChecked(PlentifulHarvest) &&
-                    (IsEnabled(CustomComboPreset.RPR_ST_SimpleMode) &&
-                     GetDebuffRemainingTime(Debuffs.DeathsDesign) <= 8 ||
-                     IsEnabled(CustomComboPreset.RPR_ST_AdvancedMode) &&
-                     GetDebuffRemainingTime(Debuffs.DeathsDesign) <= Config.RPR_SoDRefreshRange))
-                    return true;
+                    //lvl 88+ general use
+                    if (LevelChecked(PlentifulHarvest) && !HasEffect(Buffs.Enshrouded) &&
+                        GetDebuffRemainingTime(Debuffs.DeathsDesign) <= 8 &&
+                        (GetCooldownRemainingTime(ArcaneCircle) > GCD * 8 || IsOffCooldown(ArcaneCircle)))
+                        return true;
+
+                    //below lvl 88 use
+                    if (!LevelChecked(PlentifulHarvest) &&
+                        GetDebuffRemainingTime(Debuffs.DeathsDesign) <= 8)
+                        return true;
+                }
             }
 
-            if (IsEnabled(CustomComboPreset.RPR_ST_AdvancedMode) &&
-                Config.RPR_ST_ArcaneCircle_SubOption == 1 && !InBossEncounter())
+            if (IsEnabled(CustomComboPreset.RPR_ST_AdvancedMode))
             {
-                if (LevelChecked(PlentifulHarvest) && !HasEffect(Buffs.Enshrouded) &&
-                    (IsEnabled(CustomComboPreset.RPR_ST_SimpleMode) &&
-                     GetDebuffRemainingTime(Debuffs.DeathsDesign) <= 8 ||
-                     IsEnabled(CustomComboPreset.RPR_ST_AdvancedMode) &&
-                     GetDebuffRemainingTime(Debuffs.DeathsDesign) <= Config.RPR_SoDRefreshRange))
-                    return true;
+                if (Config.RPR_ST_ArcaneCircle_SubOption == 1 && !InBossEncounter())
+                {
+                    if (LevelChecked(PlentifulHarvest) && !HasEffect(Buffs.Enshrouded) &&
+                        GetDebuffRemainingTime(Debuffs.DeathsDesign) <= Config.RPR_SoDRefreshRange)
+                        return true;
+                }
+
+                if (Config.RPR_ST_ArcaneCircle_SubOption == 0 ||
+                    Config.RPR_ST_ArcaneCircle_SubOption == 1 && InBossEncounter())
+                {
+                    //1st part double enshroud
+                    if (LevelChecked(PlentifulHarvest) && HasEffect(Buffs.Enshrouded) &&
+                        GetCooldownRemainingTime(ArcaneCircle) <= GCD * 2 + 1.5 && JustUsed(Enshroud))
+                        return true;
+
+                    //2nd part double enshroud
+                    if (LevelChecked(PlentifulHarvest) && HasEffect(Buffs.Enshrouded) &&
+                        (GetCooldownRemainingTime(ArcaneCircle) <= GCD || IsOffCooldown(ArcaneCircle)) &&
+                        (JustUsed(VoidReaping) || JustUsed(CrossReaping)))
+                        return true;
+
+                    //lvl 88+ general use
+                    if (LevelChecked(PlentifulHarvest) && !HasEffect(Buffs.Enshrouded) &&
+                        GetDebuffRemainingTime(Debuffs.DeathsDesign) <= Config.RPR_SoDRefreshRange &&
+                        (GetCooldownRemainingTime(ArcaneCircle) > GCD * 8 || IsOffCooldown(ArcaneCircle)))
+                        return true;
+
+                    //below lvl 88 use
+                    if (!LevelChecked(PlentifulHarvest) &&
+                        GetDebuffRemainingTime(Debuffs.DeathsDesign) <= Config.RPR_SoDRefreshRange)
+                        return true;
+                }
             }
         }
 
