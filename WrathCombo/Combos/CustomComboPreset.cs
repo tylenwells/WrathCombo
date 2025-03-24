241be6928b34635e62a276855f956d99fdec0485
ace <167795370+Akechi-kun@users.noreply.github.com>
2025-03-24 02:27:30 -0700

    `GNB` scuffed options

--- WrathCombo/Combos/CustomComboPreset.cs
+++ WrathCombo/Combos/CustomComboPreset.cs
@@ -2201,6 +2201,10 @@ public enum CustomComboPreset
     GNB_ST_Overcap = 7018,
 
     [ParentCombo(GNB_ST_Advanced)]
+    [CustomComboInfo("Scuffed Option", "Adds Solid Barrel into the rotation under scuffed conditions:\n- No Mercy is active\n- 1 cartridge\n- Last combo action was Brutal Shell\n- Gnashing Fang is still on cooldown", GNB.JobID)]
+    GNB_ST_Scuffed = 7372,
+
+    [ParentCombo(GNB_ST_Advanced)]
     [CustomComboInfo("Lightning Shot Uptime Option", "Adds Lightning Shot to the main combo when you are out of range.",
         GNB.JobID)]
     GNB_ST_RangedUptime = 7004,
@@ -2449,6 +2453,10 @@ public enum CustomComboPreset
     [ParentCombo(GNB_GF_Features)]
     [CustomComboInfo("Burst Strike Option", "Adds Burst Strike on Gnashing Fang under No Mercy when appropriate.", GNB.JobID)]
     GNB_GF_BurstStrike = 7309,
+
+    [ParentCombo(GNB_GF_Features)]
+    [CustomComboInfo("Scuffed Option", "Adds Solid Barrel to Gnashing Fang under scuffed conditions:\n- 1 cartridge\n- No Mercy is active\n- Last combo action was Brutal Shell\n- Gnashing Fang is still on cooldown", GNB.JobID)]
+    GNB_GF_Scuffed = 7371,
     #endregion
 
     #region No Mercy
